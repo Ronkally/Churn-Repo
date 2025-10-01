0cc6df8f1adeec608618e65fb9175fa285429319
Ronkally <jacobmunozr@outlook.com>
2025-10-01 16:49:11 -0700

    Create math.js with basic arithmetic functions

---
+++ math.js
@@ -0,0 +1,28 @@
+// math.js
+
+/**
+ * Adds two numbers together
+ */
+function add(a, b) {
+  return a + b;
+}
+
+/**
+ * Subtracts the second number from the first
+ */
+function subtract(a, b) {
+  return a - b;
+}
+
+/**
+ * Multiplies two numbers
+ */
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+};
